1baf8a744dd34dba31ebe0c15988deafa90113ba
Steven Harris <stevenharrisdev@gmail.com>
2024-07-05 23:41:55 -0500

    add api package

--- main.go
+++ main.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	api "github.com/steven-harris/github-monitor/api"
 )
 
 func main() {
 	handleSigTerms()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		err := tmpl.Execute(w, data)
-		if err != nil {
-			log.Fatalf("Could not load index.html")
-		}
-	})
-
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Healthy")
-	})
-
+	api.AddRoutes()
 	fmt.Printf("Server starting on port 8888")
-
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
